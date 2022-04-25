0aebf2dd4e54748c7a854c62eb475667b05a11e0
laptop_ubuntu <frankensteinquasimo@outlook.com>
2022-04-26 06:05:21 +0800

    ok

--- thirdparty/cmake_script/build_external_project.cmake
+++ thirdparty/cmake_script/build_external_project.cmake
@@ -24,13 +24,13 @@ function (build_external_project target prefix url) #FOLLOWING ARGUMENTS are the
     ")
 
     file(WRITE ${trigger_build_target}/CMakeLists.txt "${CMAKE_LIST_CONTENT}")
-    message("TAG:thirdparty cmake_script build_external_project-CMakelists--\n${CMAKE_LIST_CONTENT}")
+    message("TAG:thirdparty cmake_script build_external_project-CMakelists--\n${CMAKE_LIST_CONTENT}\ntrigger_build_target:${trigger_build_target}")
 
-    execute_process(COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} ..
-        WORKING_DIRECTORY ${trigger_build_target}/build
+    execute_process(COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} 
+        WORKING_DIRECTORY ${trigger_build_target}
         )
-    execute_process(COMMAND ${CMAKE_COMMAND} --build . -j8
-        WORKING_DIRECTORY ${trigger_build_target}/build
+    execute_process(COMMAND ${CMAKE_COMMAND} --build . -j12
+        WORKING_DIRECTORY ${trigger_build_target}
         )
 
 endfunction()
